9f0978f658b2c8520281848b993a41395a3be969
pavlo <velykyi305@gmail.com>
2018-03-14 00:43:45 +0200

    add  solution Task4

--- src/com/velykyi/Main.java
+++ src/com/velykyi/Main.java
@@ -1,8 +1,16 @@
 package com.velykyi;
 
 public class Main {
+    public void f(Integer a){
+        System.out.println( " integer");
+    }
+    public void f(Object a){
+        System.out.println( " Object");
+    }
 
     public static void main(String[] args) {
 	// write your code here
+        Main main = new Main();
+        main.f(new Double(5));
     }
 }
